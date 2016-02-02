47d2bae4e8ae23aceae73d8717609552ee697a4f
himanishk <hkushary@mediamath.com>
2016-02-02 14:39:18 -0500

    Sample Config trait

--- src/main/scala/com/hk/test/LocalConfigSettings.scala
+++ src/main/scala/com/hk/test/LocalConfigSettings.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.ConfigFactory
   * Copyright (C) 2014 MediaMath <http://www.mediamath.com> 
   * Created by hkushary on 2/2/16.
   */
-trait ConfigSettings {
+trait LocalConfigSettings {
   def environmentStr = sys.props.get("MM_EXEC_ENV").getOrElse("test").toLowerCase
   def config = ConfigFactory.load(environmentStr)
 }
